abac7e486902da4d19322ec27369aca5f8e3b9de
George-Rafhael-Dev <georgerafhael113@gmail.com>
2026-05-15 06:10:12 -0300

    chore(docker): add MySQL service and pdo_mysql extension to Docker setup

--- dockerfile
+++ dockerfile
@@ -1,21 +1,19 @@
 FROM php:8.2-cli
 
-# Instala dependências do sistema
 RUN apt-get update && apt-get install -y \
     unzip \
     git \
-    curl
+    curl \
+    netcat-openbsd
 
-# Instala composer
+RUN docker-php-ext-install pdo pdo_mysql
+
 COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
 
-# Define diretório
 WORKDIR /app
 
-# Copia projeto
 COPY . .
 
-# Permissão pro script
 RUN chmod +x entrypoint.sh
 
 ENTRYPOINT ["sh", "entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,24 +5,30 @@ set -e
 
 echo "Iniciando container..."
 
-# cria .env se não existir
 if [ ! -f .env ]; then
   echo "Criando .env..."
   cp .env.example .env
 fi
 
-# instala dependências se não existir vendor
 if [ ! -d vendor ]; then
   echo "Instalando dependências (composer install)..."
   composer install
 fi
 
-# gera key só se não tiver
 if ! grep -q "^APP_KEY=base64:" .env; then
   echo "Gerando APP_KEY..."
   php artisan key:generate
 fi
 
-# inicia servidor
+echo "Aguardando banco de dados..."
+until nc -z db 3306; do
+    echo "Banco ainda não disponível, aguardando..."
+    sleep 2
+done
+echo "Banco disponível."
+
+echo "Rodando migrations..."
+php artisan migrate
+
 echo "Subindo Laravel..."
 exec php artisan serve --host=0.0.0.0 --port=8000
